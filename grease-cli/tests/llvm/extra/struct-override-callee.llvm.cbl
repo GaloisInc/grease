afe3997ccf1382cc48725e20508cd67a0eceebde
Langston Barrett <langston.barrett@gmail.com>
2025-04-17 21:35:33 -0400

    grease-cli: Remove stray `CHECK` lines

--- grease-cli/tests/llvm/extra/struct-override-callee.llvm.cbl
+++ grease-cli/tests/llvm/extra/struct-override-callee.llvm.cbl
@@ -3,4 +3,3 @@
 (defun @struct-override-callee () (Struct (Bitvector 32) (Bitvector 64))
   (start start:
     (return (struct (bv 32 0) (bv 64 0)))))
-; CHECK: All goals passed!
--- grease-cli/tests/llvm/skip-void.llvm.cbl
+++ grease-cli/tests/llvm/skip-void.llvm.cbl
@@ -3,7 +3,7 @@
 (declare @skippable () Unit)
 (defun @test ((b (Ptr 1))) (Ptr 1)
   (start start:
-    ; CHECK: Invoking the 'skippable' function
+;; check "Invoking the 'skippable' function"
     (let _ (funcall @skippable))
     (return b)))
 ;; ok()
