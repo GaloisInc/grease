88b54c98adea6845b6b5b3d20618f0f3a2810294
Langston Barrett <langston.barrett@gmail.com>
2025-04-25 10:15:08 -0400

    Migrate Lua prelude to a separate file

---
+++ grease-cli/tests/test.lua
@@ -0,0 +1,12 @@
+function ok() check 'All goals passed!' end
+
+function must_fail() check 'Likely bug: unavoidable error' end
+
+function next_line_must_fail()
+  must_fail()
+  check(string.format('%s:%d', file(), src_line(1) + 1))
+end
+
+function no_heuristic() check 'Unable to find a heuristic for any goal' end
+
+function uninit_stack() check 'Likely bug: uninitialized stack read' end
