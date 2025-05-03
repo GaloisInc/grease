1a5685eb8f1929830d51a9281c22d2272e159082
Langston Barrett <langston.barrett@gmail.com>
2025-05-03 10:54:38 -0400

    Combine {func-ptr,-error} test cases

--- grease-cli/tests/llvm/func-ptr.llvm.cbl
+++ grease-cli/tests/llvm/func-ptr.llvm.cbl
@@ -1,12 +1,24 @@
 ; Copyright (c) Galois, Inc. 2024
 
+; Test the behavior of calls through symbolic function pointers. By default,
+; such calls are simply skipped, but --error-symbolic-fun-calls turns them
+; into a hard error.
+
 ;; flags {"--symbol", "test"}
 ;; go(prog)
+;; check "Skipped call to a symbolic function handle"
+;; ok()
+
+;; flags {"--symbol", "test"}
+;; flags {"--error-symbolic-fun-calls"}
+;; go(prog)
+;; check "Goal failed:"
+;; check "Failed to load function handle"
+;; check "Cannot resolve a symbolic pointer to a function handle"
+;; no_heuristic()
 
 (defun @test ((p (Ptr 64))) Unit
   (start start:
-;; check "Skipped call to a symbolic function handle"
     (let h (load-handle Unit () p))
     (funcall h)
     (return ())))
-;; ok()
