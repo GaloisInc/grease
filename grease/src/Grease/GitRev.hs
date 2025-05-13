0e0d3396a9b9eb59d010d1a93ea38d9bdf99b6d6
Vladislav Sabanov <email@email.com>
2025-05-13 19:07:30 -0300

    Use tGitInfoCwdTry

--- grease/src/Grease/GitRev.hs
+++ grease/src/Grease/GitRev.hs
@@ -1,24 +1,29 @@
-{-|
-Copyright        : (c) Galois, Inc. 2024
-Maintainer       : GREASE Maintainers <grease@galois.com>
--}
-
 {-# LANGUAGE TemplateHaskell #-}
 
--- | These are placed in their own module to minimize the cost of recompilation
+-- \| These are placed in their own module to minimize the cost of recompilation
 -- due to Template Haskell.
+
+-- |
+-- Copyright        : (c) Galois, Inc. 2024
+-- Maintainer       : GREASE Maintainers <grease@galois.com>
 module Grease.GitRev (hash, branch, dirty) where
 
-import GitHash (GitInfo, tGitInfoCwd, giHash, giBranch, giDirty)
+import GitHash (GitInfo, giBranch, giDirty, giHash, tGitInfoCwdTry)
 
-gitInfo :: GitInfo
-gitInfo = $$tGitInfoCwd
+gitInfo :: Either String GitInfo
+gitInfo = $$tGitInfoCwdTry
 
 hash :: String
-hash = giHash gitInfo
+hash = case gitInfo of
+    Left _ -> "UNKNOWN"
+    Right gi -> giHash gi
 
 branch :: String
-branch = giBranch gitInfo
+branch = case gitInfo of
+    Left _ -> "UNKNOWN"
+    Right gi -> giBranch gi
 
 dirty :: Bool
-dirty = giDirty gitInfo
+dirty = case gitInfo of
+  Left _ -> False
+  Right gi -> giDirty gi
