4a568179906201ed0da5d646b00edb0e2f6fe255
Langston Barrett <langston.barrett@gmail.com>
2025-05-28 13:48:00 -0400

    Change I/O tests to `declare` their overrides

--- grease-cli/tests/llvm/conc-fs.llvm.cbl
+++ grease-cli/tests/llvm/conc-fs.llvm.cbl
@@ -8,28 +8,28 @@
 
 (declare @write-c-string ((dst (Ptr 64)) (src (String Unicode))) Unit)
 
+; int open(const char *pathname, int flags, ... /* mode_t mode */ );
+(declare @open ((pathname (Ptr 64)) (flags (Ptr 32))) (Ptr 32))
+
+; ssize_t read(int fildes, void *buf, size_t nbyte);
+(declare @read ((filedes (Ptr 32)) (buf (Ptr 64)) (nbyte (Ptr 64))) (Ptr 64))
+
 (defun @test () Unit
   (start start:
 
     (let p (alloca none (bv 64 6)))
     (funcall @write-c-string p "/symb")
 
-    ; int open(const char *pathname, int flags, ... /* mode_t mode */ );
-    (let open-global (resolve-global "open"))
-    (let open (load-handle (Ptr 32) ((Ptr 64) (Ptr 32)) open-global))
     (let zero (ptr 32 0 (bv 32 0)))
-    (let fd (funcall open p zero))
+    (let fd (funcall @open p zero))
 
     ; should find existing file
     (let off (ptr-offset 32 fd))
     (assert! (not (equal? off (bv 32 0xffffffff))) "open() returned -1!")
 
-    ; ssize_t read(int fildes, void *buf, size_t nbyte);
-    (let read-global (resolve-global "read"))
-    (let read (load-handle (Ptr 64) ((Ptr 32) (Ptr 64) (Ptr 64)) read-global))
     (let buf (alloca none (bv 64 1)))
     (let one (ptr 64 0 (bv 64 1)))
-    (let _ (funcall read fd buf one))
+    (let _ (funcall @read fd buf one))
 
     (let byte (load none i8 buf))
     (let byte-off (ptr-offset 8 byte))
--- grease-cli/tests/llvm/io-overrides-fs.llvm.cbl
+++ grease-cli/tests/llvm/io-overrides-fs.llvm.cbl
@@ -8,36 +8,36 @@
 
 (declare @write-c-string ((dst (Ptr 64)) (src (String Unicode))) Unit)
 
+; int close(int filedes)
+(declare @close ((filedes (Ptr 32))) (Ptr 32))
+
+; int open(const char *pathname, int flags, ... /* mode_t mode */ );
+(declare @open ((pathname (Ptr 64)) (flags (Ptr 32))) (Ptr 32))
+
+; ssize_t read(int fildes, void *buf, size_t nbyte);
+(declare @read ((filedes (Ptr 32)) (buf (Ptr 64)) (nbyte (Ptr 64))) (Ptr 64))
+
 (defun @test () Unit
   (start start:
 
     (let p (alloca none (bv 64 6)))
     (funcall @write-c-string p "/symb")
 
-    ; int open(const char *pathname, int flags, ... /* mode_t mode */ );
-    (let open-global (resolve-global "open"))
-    (let open (load-handle (Ptr 32) ((Ptr 64) (Ptr 32)) open-global))
     (let zero (ptr 32 0 (bv 32 0)))
-    (let fd (funcall open p zero))
+    (let fd (funcall @open p zero))
 
     ; should find existing file
     (let off (ptr-offset 32 fd))
     (assert! (not (equal? off (bv 32 0xffffffff))) "open() returned -1!")
 
-    ; ssize_t read(int fildes, void *buf, size_t nbyte);
-    (let read-global (resolve-global "read"))
-    (let read (load-handle (Ptr 64) ((Ptr 32) (Ptr 64) (Ptr 64)) read-global))
     (let buf (alloca none (bv 64 1)))
     (let one (ptr 64 0 (bv 64 1)))
-    (let _ (funcall read fd buf one))
+    (let _ (funcall @read fd buf one))
 
-    ; int close(int filedes)
-    (let close-global (resolve-global "close"))
-    (let close (load-handle (Ptr 32) ((Ptr 32)) close-global))
-    (let closed1 (funcall close fd))
+    (let closed1 (funcall @close fd))
     (let closed1-off (ptr-offset 32 closed1))
     (assert! (not (equal? closed1-off (bv 32 0xffffffff))) "close() returned -1!")
-    (let closed2 (funcall close fd))
+    (let closed2 (funcall @close fd))
     (let closed2-off (ptr-offset 32 closed2))
     (assert! (equal? closed2-off (bv 32 0xffffffff)) "close() didn't return -1!")
 
