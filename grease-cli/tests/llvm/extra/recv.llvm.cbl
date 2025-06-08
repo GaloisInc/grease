04f0cd32aa443452e59e8c94dec9c9278dbe02b9
Langston Barrett <langston.barrett@gmail.com>
2025-06-08 15:10:10 -0400

    doc: More realistic override for `recv`
    Fixes #211.

--- grease-cli/tests/llvm/extra/recv.llvm.cbl
+++ grease-cli/tests/llvm/extra/recv.llvm.cbl
@@ -8,10 +8,29 @@
 
 ; `man 2 recv`: ssize_t recv(int socket, void *buffer, size_t length, int flags)
 (defun @recv ((socket (Ptr 32)) (buffer (Ptr 64)) (length (Ptr 64)) (flags (Ptr 32))) (Ptr 64)
+  ; This is a bit more awkward than it needs to be, because there are no
+  ; pointer<->integer<->nat conversions in crucible-syntax.
+  (registers
+    ($ctr (Bitvector 64))  ; loop counter (going down)
+    ($idx Nat)             ; index into vector of bytes
+    ($ptr (Ptr 64)))       ; pointer to write the next byte into
   (start start:
     (let length-bv (ptr-offset 64 length))
     (let bytes (funcall @fresh-bytes "recv" length-bv))
-    (let byte (vector-get bytes 0))
+
+    (set-register! $ctr length-bv)
+    (set-register! $idx 0)
+    (set-register! $ptr buffer)
+    (jump loop:))
+  (defblock loop:
+    (let byte (vector-get bytes $idx))
     (let byte-ptr (ptr 8 0 byte))
-    (store none i8 buffer byte-ptr)
+    (store none i8 $ptr byte-ptr)
+
+    (set-register! $ctr (- $ctr (bv 64 1)))
+    (set-register! $idx (+ $idx 1))
+    (let ptr (ptr-add-offset $ptr (bv 64 1)))
+    (set-register! $ptr ptr)
+    (branch (equal? $ctr (bv 64 0)) end: loop:))
+  (defblock end:
     (return length)))
