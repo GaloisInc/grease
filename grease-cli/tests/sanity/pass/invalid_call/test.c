3f8a6f1b9ed5835adad6d7ecb8d82afa03984f69
2over12 <ismith@.galois.com>
2025-06-16 14:39:53 -0400

    unfiy tests

--- grease-cli/tests/sanity/pass/invalid_call/test.c
+++ grease-cli/tests/sanity/pass/invalid_call/test.c
@@ -1,7 +1,5 @@
 /* Copyright (c) Galois, Inc. 2025 */
 
-// all: flags {"--symbol", "test"}
-// all: go(prog)
 
 int test() {
     long x = 0xDEADBEEF;
@@ -9,8 +7,15 @@ int test() {
     return fptr() + 1;
 }
 
-
+// all: flags {"--symbol", "test"}
+// all: go(prog)
 // arm: check "Invalid address: 0xdeadbeef"
 // on ppc the last two bits are cleared
 // ppc32: check "Invalid address: 0xdeadbeec"
 // x64: check "Invalid address: 0xdeadbeef"
+
+// all: flags {"--symbol", "test", "--skip-invalid-call-addrs"}
+// all: go(prog)
+// arm: check "TopV where PSTATE_T expected"
+// x64: ok()
+// ppc32: ok()
