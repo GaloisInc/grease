184afe2a58b9f99bb1fe8bed8c2b3097ccdfcf84
2over12 <ismith@.galois.com>
2025-06-19 13:05:27 -0400

    fix comment

--- grease-cli/tests/Shape.hs
+++ grease-cli/tests/Shape.hs
@@ -85,8 +85,8 @@ eqMemShape x y = x == y
 -- That is 'PtrTarget' Nothing, 'PtrTarget' Nothing will result in a print of
 -- 0: x
 -- 1: y
+-- The parser will then parse the blockIDs as 0 and 1 accordingly.
 
-The parser will then parse the blockIDs as 0 and 1 accordingly.  -}
 eqPtrTarget :: PtrShape.PtrTarget w NoTag -> PtrShape.PtrTarget w NoTag  -> Bool
 eqPtrTarget (PtrShape.PtrTarget _ mems)  (PtrShape.PtrTarget _ mems') =
   Seq.length mems == Seq.length mems' &&
