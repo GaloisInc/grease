bdd635a65cbeebf5e329a823904d2040ebb66d81
2over12 <ismith@.galois.com>
2025-06-19 09:52:54 -0400

    add alias check

--- grease-cli/tests/x86/aliased-pointer-shape.x64.cbl
+++ grease-cli/tests/x86/aliased-pointer-shape.x64.cbl
@@ -15,8 +15,11 @@
     (let rsi-ptr (pointer-read (Ptr 64) le rsi))
     (let firstbyte (pointer-read (Bitvector 8) le rdi-ptr))
     (let lastbyte (pointer-read (Bitvector 8) le rsi-ptr))
+    (let maybe-rsi-ptr (pointer-add rdi-ptr (bv 64 3)))
+    (let is-eq-ptrs (pointer-eq rsi-ptr maybe-rsi-ptr))
     (assert! (equal? firstbyte (bv 8 222)) "Value not de")
     (assert! (equal? lastbyte (bv 8 239)) "Value not ef")
+    (assert! is-eq-ptrs "The pointers should be aliased to the same block")
     (return regs)))
 
 ;; ok()
