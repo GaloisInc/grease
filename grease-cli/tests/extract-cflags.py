17b43d6806eb5c677676a20c5119813a044680bc
Langston Barrett <langston.barrett@gmail.com>
2025-06-26 14:51:18 -0400

    tests: Port CFLAGS extraction script to Python
    To enable more complex features in the future.

---
+++ grease-cli/tests/extract-cflags.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+#
+# Extract C compiler flags embedded in test files. See `doc/dev/tests.md` and
+# the Makefile.
+
+from argparse import ArgumentParser
+from pathlib import Path
+from sys import exit, stderr
+
+
+COMMENT = "// CFLAGS: "
+
+
+def eprint(*args, **kwargs):
+    print(*args, file=stderr, **kwargs)
+
+
+def die(msg):
+    eprint(msg)
+    exit(1)
+
+
+def gather(txt):
+    raw_flags = []
+    for line in txt.splitlines():
+        if line.startswith(COMMENT):
+            flags = line.removeprefix(COMMENT)
+            raw_flags.append(flags)
+    return raw_flags
+
+
+def extract(path):
+    if not path.is_file():
+        die("not a file: {path}")
+
+    raw_flags = gather(path.read_text())
+    return " ".join(raw_flags)
+
+
+parser = ArgumentParser()
+parser.add_argument("file", type=Path)
+args = parser.parse_args()
+print(extract(args.file))
--- grease-cli/tests/extract-cflags.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Extract C compiler flags embedded in test files. See `doc/dev/tests.md` and
-# the Makefile.
-
-set -eu
-
-grep '^// CFLAGS: ' "${1}" | sed 's|^// CFLAGS: ||'
