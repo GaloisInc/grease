edd4c4c5aa077207ea8e6980088749ffaa3ea9ea
2over12 <ismith@galois.com>
2025-06-30 12:23:41 -0400

    add test source

---
+++ grease-cli/tests/sanity/pass/dwarf-shapes/test.c
@@ -0,0 +1,9 @@
+struct bar {
+  int x;
+};
+
+int foo(int x, int *y, struct bar *z) { return x + *y + z->x; }
+
+// all: flags {"--symbol", "foo", "--enable-dwarf-preconditions", " --no-heuristics"}
+// x64: go(prog)
+// x64: ok()
