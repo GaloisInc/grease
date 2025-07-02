eccb6de73be593f471251ac0846b6420e490f4d0
Langston Barrett <langston.barrett@gmail.com>
2025-07-02 12:52:24 -0400

    llvm: Add a test for supporting all LLVM types

---
+++ grease-cli/tests/llvm/types.llvm.cbl
@@ -0,0 +1,31 @@
+; Copyright (c) Galois, Inc. 2025
+
+; Test that GREASE handles all the Crucible types targeted by Crucible-LLVM.
+
+;; flags {"--symbol", "test"}
+;; go(prog)
+
+(defun @test
+  ((x0 Bool)
+   (x1 (Ptr 1))
+   (x2 (Ptr 8))
+   (x3 (Ptr 16))
+   (x4 (Ptr 32))
+   (x5 (Ptr 64))
+   (x6 (Ptr 128))
+   (x7 (Ptr 256))
+   (x8 (Ptr 512))
+   (x9 (FP Half))
+   (x10 (FP Float))
+   (x11 (FP Double))
+   (x12 (FP Quad))
+   (x13 (FP DoubleDouble))
+   (x14 (FP X86_80))
+   (x15 (Struct Bool Unit))
+   (x16 Unit))
+  Unit
+  (start start:
+    (return ())))
+
+; TODO(#268): Support floats
+;; check "Exception: Could not determine minimal shape for argument"
