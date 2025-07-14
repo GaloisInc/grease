0f0c1541f2b8d6f84355382b6baec0e140e095ba
Langston Barrett <langston.barrett@gmail.com>
2025-07-14 09:35:12 -1000

    Add a regression test for #247 (which is still open)

---
+++ grease-cli/tests/x86/T247-cex-print-wrong-order.x64.cbl
@@ -0,0 +1,30 @@
+; Copyright (c) Galois, Inc. 2025
+
+; Regression test for #247, wherein GREASE prints the contents of an allocation
+; in the wrong order as part of a counterexample.
+
+;; flags {"--symbol", "test"}
+;; flags {"--initial-precondition", "tests/x86/extra/T247-cex-print-wrong-order.txt"}
+;; go(prog)
+
+(declare @abort () Unit)
+
+(defun @test ((regs X86Regs)) X86Regs
+  (start start:
+    (let buf (get-reg rdi regs))
+    (let large-offset (bv 64 0xff))
+    (let buf-large-offset (pointer-add buf large-offset))
+    (let buf-contents (pointer-read (Bitvector 8) le buf-large-offset))
+    (branch (equal? buf-contents (bv 8 42)) abort: no-abort:))
+
+  (defblock abort:
+    (funcall @abort)
+    (return regs))
+
+  (defblock no-abort:
+    (return regs)))
+
+; TODO(#247): Why is this printed in reverse order?
+;; check [[
+;; 2a ##*ff
+;; ]]
