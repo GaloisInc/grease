d073ecedd56b8114cb5ed6e72e85126cf896c843
2over12 <ismith@galois.com>
2025-08-03 16:02:46 -0400

    fix flag

--- grease-cli/tests/x86/write-to-fixed-address-in-rax.x64.cbl
+++ grease-cli/tests/x86/write-to-fixed-address-in-rax.x64.cbl
@@ -2,7 +2,7 @@
 
 ; A test case that writes to a fixed unmapped address we should not try to refine.
 
-;; flags {"--symbol", "test", "--arg-u64 rax:0x0bad1deadeadbeef"}
+;; flags {"--symbol", "test", "--arg-u64", "rax:0x0bad1deadeadbeef"}
 ;; go(prog)
 
 (defun @test ((regs X86Regs)) X86Regs
