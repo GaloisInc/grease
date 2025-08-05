7b116c13bb8ad95fa230013fa0ae210d9a2a26b5
2over12 <ismith@galois.com>
2025-08-05 19:19:33 -0400

    add pretty printer

--- grease/src/Grease/ErrorDescription.hs
+++ grease/src/Grease/ErrorDescription.hs
@@ -1,12 +1,18 @@
-module Grease.ErrorDescription (ErrorDescription (..), concretizeErrorDescription) where
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module Grease.ErrorDescription (ErrorDescription (..), concretizeErrorDescription, ppMacawError, ppErrorDesc) where
 
 import Data.Macaw.Symbolic.Memory (MacawError (UnmappedGlobalMemoryAccess))
 import Data.Macaw.Symbolic.Memory qualified as MSM
 import Lang.Crucible.LLVM.Errors qualified as CLLVM
 import Lang.Crucible.LLVM.Errors qualified as Mem
+import Lang.Crucible.LLVM.MemModel qualified as CLLVM
 import Lang.Crucible.LLVM.MemModel.CallStack qualified as LLCS
 import Lang.Crucible.LLVM.MemModel.Pointer qualified as Mem
+import Prettyprinter qualified as PP
 import What4.Expr qualified as W4
+import What4.Interface qualified as W4
 import What4.Interface qualified as WI
 
 concretizeErrorDescription :: (WI.IsExprBuilder sym, sym ~ W4.ExprBuilder t st fs) => sym -> W4.GroundEvalFn t -> ErrorDescription sym -> IO (ErrorDescription sym)
@@ -23,3 +29,22 @@ concretizeErrorDescription sym (W4.GroundEvalFn gFn) (MacawMemError memerr) = do
 data ErrorDescription sym
   = CrucibleLLVMError (CLLVM.BadBehavior sym) LLCS.CallStack
   | MacawMemError (MSM.MacawError sym)
+
+ppDelimitedObj :: [PP.Doc a] -> PP.Doc a
+ppDelimitedObj lst = PP.sep $ "(" : lst ++ [")"]
+
+-- TODO(#310) Pretty print macaw error using a macaw implementation
+ppMacawError :: W4.IsExpr (W4.SymExpr sym) => MSM.MacawError sym -> PP.Doc a
+ppMacawError (MSM.UnmappedGlobalMemoryAccess ptrVal) = PP.sep ["(", "UnmappedGlobalMemoryAccess", CLLVM.ppPtr ptrVal, ")"]
+
+ppErrorDesc :: W4.IsExpr (W4.SymExpr sym) => ErrorDescription sym -> PP.Doc a
+ppErrorDesc (MacawMemError mmErr) = ppDelimitedObj ["MacawMemErr", ppMacawError mmErr]
+ppErrorDesc (CrucibleLLVMError badBehavior cs) = ppDelimitedObj ["CrucibleLLVMError", CLLVM.ppBB badBehavior, LLCS.ppCallStack cs]
+
+instance W4.IsExpr (W4.SymExpr sym) => Show (ErrorDescription sym) where
+  show :: WI.IsExpr (WI.SymExpr sym) => ErrorDescription sym -> String
+  show = show . ppErrorDesc
+
+instance W4.IsExpr (W4.SymExpr sym) => PP.Pretty (ErrorDescription sym) where
+  pretty :: WI.IsExpr (WI.SymExpr sym) => ErrorDescription sym -> PP.Doc ann
+  pretty = PP.viaShow
