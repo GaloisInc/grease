9379af83040277f74f54b7b27b9f58e9ee6c3420
Langston Barrett <langston.barrett@gmail.com>
2025-08-14 15:44:11 -0400

    Add a test for path splitting, fix bug with bounded execution

---
+++ grease-cli/tests/llvm/uninit-stack-conditional.llvm.cbl
@@ -0,0 +1,24 @@
+; Copyright (c) Galois, Inc. 2024
+
+; Test that SSE and DFS strategies give the same result.
+
+(defun @test ((b Bool)) Unit
+  (start start:
+    (let sz (bv 64 1))
+    (let a (alloca none sz))
+    (branch b if: else:))
+  (defblock if:
+    (return ()))
+  (defblock else:
+    (load none i8 a)
+    (return ())))
+
+;; flags {"--symbol", "test"}
+;; flags {"--path-strategy", "Sse"}
+;; go(prog)
+;; uninit_stack()
+
+;; flags {"--symbol", "test"}
+;; flags {"--path-strategy", "Dfs"}
+;; go(prog)
+;; uninit_stack()
