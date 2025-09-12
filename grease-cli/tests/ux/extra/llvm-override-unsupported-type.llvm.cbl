7102aa4cb6e38f48b1bfd34853a904089e513b17
Langston Barrett <langston.barrett@gmail.com>
2025-09-12 11:30:19 -0400

    Add tests for LLVM S-expression override errors

---
+++ grease-cli/tests/ux/extra/llvm-override-unsupported-type.llvm.cbl
@@ -0,0 +1,3 @@
+(defun @llvm-override-unsupported-type ((x (Sequence Integer))) Unit
+  (start start:
+    (return ())))
---
+++ grease-cli/tests/ux/llvm-override-unsupported-type.lua
@@ -0,0 +1,7 @@
+-- Test the UX of our error messages for unsupported types in LLVM overrides
+
+path = "tests/ux/extra/llvm-override-unsupported-type.llvm.cbl"
+flags {"--symbol", "test", "--overrides", path}
+go "tests/llvm/load.llvm.cbl"
+exception()
+check("user error: Unsupported type in override in file " .. path)
