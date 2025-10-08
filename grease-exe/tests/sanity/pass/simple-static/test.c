a8573a04bef1ccdf07e394d4d43328b9e933f5dd
2over12 <ismith@galois.com>
2025-10-08 16:23:18 -0400

    Print address error in hex (#421)
    Hex print

--- grease-exe/tests/sanity/pass/simple-static/test.c
+++ grease-exe/tests/sanity/pass/simple-static/test.c
@@ -11,3 +11,7 @@ int test(int *x) { return *x; }
 // arm: flags {"--load-base", "0x1000", "--symbol", "test", "--raw-binary"}
 // arm: go(prog)
 // arm: check("An entrypoint was provided that was not an address")
+
+// arm: flags {"--load-base", "0x1000", "--address", "0x800000", "--raw-binary"}
+// arm: go(prog)
+// arm: check("A provided address entrypoint could not be resolved to an address in memory: 0x800000")
