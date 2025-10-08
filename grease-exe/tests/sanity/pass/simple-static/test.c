9a65048159d19757af8d5a015597dd3d105e38d9
2over12 <ismith@galois.com>
2025-10-08 13:42:15 -0400

    add test case

--- grease-exe/tests/sanity/pass/simple-static/test.c
+++ grease-exe/tests/sanity/pass/simple-static/test.c
@@ -7,3 +7,7 @@ int test(int *x) { return *x; }
 // all: flags {"--load-base", "0x1000", "--address", "0x1000", "--raw-binary"}
 // arm: go(prog)
 // arm: ok()
+
+// all: flags {"--load-base", "0x1000", "--symbol", "test", "--raw-binary"}
+// arm: go(prog)
+// arm: check("An entrypoint was provided that was not an address")
