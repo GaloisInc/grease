31043bc125e08a5a4f9fe15213376b72bdf1f089
Langston Barrett <langston.barrett@gmail.com>
2025-10-14 10:24:13 -0400

    cli: Unify handling of enumerations
    Previously, we had two ways of signaling possible values for
    enumerations. One was to add a parenthesized list of possible values
    to the `help` text, another was to present alternatives as part of the
    metavariable. This commit standardizes on the latter, and generally
    unifies more handling of enumerations in the CLI.

--- grease/src/Grease/Requirement.hs
+++ grease/src/Grease/Requirement.hs
@@ -4,19 +4,12 @@
 module Grease.Requirement (
   Requirement (..),
   displayReq,
-  RequirementParser,
-  reqParser,
 ) where
 
-import Control.Applicative (Alternative (..))
 import Data.Aeson qualified as Aeson
 import Data.Text (Text)
-import Data.Void (Void)
 import GHC.Generics (Generic)
 import Prettyprinter qualified as PP
-import Text.Megaparsec qualified as TM
-import Text.Megaparsec.Char qualified as TMC
-import Text.Megaparsec.Char.Lexer qualified as TMCL
 
 -- | A requirement that a software program should adhere to. This data type
 -- enumerates all of the requirements that @grease@ explicitly supports checking
@@ -45,21 +38,3 @@ displayReq :: Requirement -> Text
 displayReq = \case
   InText -> "in-text"
   NoMprotect -> "no-mprotect"
-
--- | A @megaparsec@ parser type for 'Requirement's.
-type RequirementParser = TM.Parsec Void Text
-
--- | Parse a symbol from 'TM.Tokens'.
-symbol :: TM.Tokens Text -> RequirementParser Text
-symbol = TMCL.symbol spaceConsumer
-
--- | A standard space consumer that does not support comments.
-spaceConsumer :: RequirementParser ()
-spaceConsumer = TMCL.space TMC.space1 empty empty
-
--- | Parse a 'Requirement'.
-reqParser :: RequirementParser Requirement
-reqParser = inText <|> noMprotect
- where
-  inText = symbol "in-text" *> pure InText
-  noMprotect = symbol "no-mprotect" *> pure NoMprotect
