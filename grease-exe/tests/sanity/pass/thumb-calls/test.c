934530a13cb9e1280a3fe1e3569c44bfebbe9cf2
Ian Smith <ian.smith@galois.com>
2025-10-20 20:21:34 +0000

    generate test

--- grease-exe/tests/sanity/pass/thumb-calls/test.c
+++ grease-exe/tests/sanity/pass/thumb-calls/test.c
@@ -2,8 +2,10 @@
 
 // Simple call test to make sure calling thumb mode functions works.
 
+__attribute__((target("thumb")))
 int callee(int *x) { return *x; }
 
+__attribute__((target("thumb")))
 int test(int *x) { return callee(x); }
 
 // arm: flags {"--load-base", "0x1000", "--address", "0x1001", "--raw-binary"}
