59843394848fe5d038fe99f2c36014b7ed5791e2
2over12 <ismith@galois.com>
2025-10-20 15:54:25 -0400

    thumb test case

---
+++ grease-exe/tests/sanity/pass/thumb-calls/test.c
@@ -0,0 +1,11 @@
+/* Copyright (c) Galois, Inc. 2025 */
+
+// Simple call test to make sure calling thumb mode functions works.
+
+int callee(int *x) { return *x; }
+
+int test(int *x) { return callee(x); }
+
+// arm: flags {"--load-base", "0x1000", "--address", "0x1001", "--raw-binary"}
+// arm: go(prog)
+// arm: ok()
