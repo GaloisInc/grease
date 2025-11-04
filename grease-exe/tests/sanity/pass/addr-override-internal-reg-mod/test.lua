1f5dfc44e2ef027d18fae8ec86e382d58eec1050
2over12 <ismith@galois.com>
2025-11-04 15:19:34 -0500

    update comment

--- grease-exe/tests/sanity/pass/addr-override-internal-reg-mod/test.lua
+++ grease-exe/tests/sanity/pass/addr-override-internal-reg-mod/test.lua
@@ -1,5 +1,6 @@
 flags {"--symbol", "test"}
--- Address of the `test` function from `objdump -d`.
+-- Address of the nop after setting rsi == rdi function from `objdump -d`.
 flags {"--addr-override", "0x10001003:tests/sanity/pass/addr-override-internal-reg-mod/extra/addr-override.x86_64.cbl"}
+-- We check that we can observe the change in rsi state from the middle of the block
 go "tests/sanity/pass/addr-override-internal-reg-mod/extra/test.x64.elf"
 ok()
