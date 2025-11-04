be670c461cdfbfe32c132ab37bf7f3b3bb3a1571
2over12 <ismith@galois.com>
2025-11-04 15:05:44 -0500

    fix more typos

--- grease-exe/tests/sanity/pass/addr-override-internal-reg-mod/extra/addr-override.x86_64.cbl
+++ grease-exe/tests/sanity/pass/addr-override-internal-reg-mod/extra/addr-override.x86_64.cbl
@@ -1,6 +1,7 @@
 (defun @addr-override ((regs X86Regs)) Unit
   (start start:
-    (let vrip (get-reg rip regs))
+    (let vrdi (get-reg rdi regs))
     (let vrsi (get-reg rsi regs))
-    (assert! (pointer-eq vrip vrsi) "assert that rsi and rip are equal")
+    (let iseq (pointer-eq vrdi vrsi))
+    (assert! iseq "assert that rsi and rdi are equal")
     (return ())))
--- grease-exe/tests/sanity/pass/addr-override-internal-reg-mod/test.lua
+++ grease-exe/tests/sanity/pass/addr-override-internal-reg-mod/test.lua
@@ -1,5 +1,5 @@
 flags {"--symbol", "test"}
 -- Address of the `test` function from `objdump -d`.
-flags {"--addr-override", "0x1003:tests/sanity/pass/addr-override-internal-reg-mod/extra/add-override.x64_64.cbl"}
+flags {"--addr-override", "0x10001003:tests/sanity/pass/addr-override-internal-reg-mod/extra/addr-override.x86_64.cbl"}
 go "tests/sanity/pass/addr-override-internal-reg-mod/extra/test.x64.elf"
 ok()
