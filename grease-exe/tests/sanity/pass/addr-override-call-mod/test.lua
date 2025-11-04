12cf9410df28e17a861edf26bff8542efba68c95
2over12 <ismith@galois.com>
2025-11-04 16:03:00 -0500

    fix test

--- grease-exe/tests/sanity/pass/addr-override-call-mod/test.lua
+++ grease-exe/tests/sanity/pass/addr-override-call-mod/test.lua
@@ -1,6 +1,6 @@
 flags {"--symbol", "test"}
 -- Address of the nop after setting rsi == rdi function from `objdump -d`.
-flags {"--addr-override", "0x10001024:tests/sanity/pass/addr-override-call-mod/extra/addr-override.x86_64.cbl"}
+flags {"--addr-override", "0x10001029:tests/sanity/pass/addr-override-call-mod/extra/addr-override.x86_64.cbl"}
 -- We check that we can observe the change in rsi state from the middle of the block
 go "tests/sanity/pass/addr-override-call-mod/extra/test.x64.elf"
 ok()
