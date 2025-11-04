efdb0949861b9feffd0a843872c8ef9dbb6b6fed
2over12 <ismith@galois.com>
2025-11-04 13:37:51 -0500

    move test

--- grease-exe/tests/sanity/pass/addr-override-internal-reg-mod/test.lua
+++ grease-exe/tests/sanity/pass/addr-override-internal-reg-mod/test.lua
@@ -1,5 +1,5 @@
 flags {"--symbol", "test"}
 -- Address of the `test` function from `objdump -d`.
-flags {"--addr-override", "0x1003:tests/sanity/pass/addr-override-internal-reg-mod/add-override.x64_64.cbl"}
-go "tests/sanity/pass/addr-override-internal-reg-mod/test.x64.elf"
+flags {"--addr-override", "0x1003:tests/sanity/pass/addr-override-internal-reg-mod/extra/add-override.x64_64.cbl"}
+go "tests/sanity/pass/addr-override-internal-reg-mod/extra/test.x64.elf"
 ok()
