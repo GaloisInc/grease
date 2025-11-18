c660f145566431a76684b70a4b5322bd09ab85ee
Langston Barrett <langston.barrett@gmail.com>
2025-11-18 09:16:33 -0500

    Documentation for Ninja generator
    Fixes #459.

--- scripts/lint/lint.py
+++ scripts/lint/lint.py
@@ -2,6 +2,46 @@
 
 """Run linters incrementally and in parallel using Ninja (see ./doc/dev.md)"""
 
+# * Ninja
+#
+# Ninja is essentially a simpler, faster version of Make. A Ninja configuration
+# consists of *rules* (`rule`) and some number of *build statements* (`build`).
+# A rule is an abbreviation for a shell command, and a build statement is a
+# recipe for producing some number of output files (*targets*) from some number
+# of input files by running a rule.
+#
+# Conceptually, we can consider the Ninja configuration as a hypergraph where
+# the nodes are files and the hyperedges are build statements, labeled by rules.
+# The inputs to Ninja are this hypergraph and a set of desired targets. Ninja
+# traverses the hypergraph and recursively runs rules to build everything until
+# it can build the desired targets.
+#
+# Just like Make, Ninja checks *file modification times* to see if rebuilding
+# is necessary. If the output was modified more recently than all of the inputs
+# (according to filesystem metadata), then Ninja will skip rebuilding that
+# target.
+#
+# See ninja.build for more information about Ninja.
+#
+# * This script
+#
+# This script generates Ninja configurations to run linters. It works by
+# generating build statements that produce one target file (in `.out/`) for each
+# combination of linter and input files. For example, for a file `foo/bar.py`
+# and the linter `ruff`, it would generate a `build` statement like
+#
+#     build .out/foo-bar.py: ruff-check foo/bar.py
+#
+# where the `ruff-check` rule is something like:
+#
+#     rule ruff-check
+#       command = ruff check -- $in && touch $out
+#
+# Together, these say "to produce output file `.out/foo-bar.py`, run `ruff
+# check` on `foo/bar.py` and then (if it succeeds) `touch foo-bar.py`". Hence,
+# the rules produce empty output files in `.out/` indicating that the linter has
+# been run.
+
 from argparse import ArgumentParser
 from os import execvp
 from pathlib import Path
