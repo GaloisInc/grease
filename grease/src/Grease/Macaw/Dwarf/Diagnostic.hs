73c08a99906031080e4f306edc746b0ec5988084
2over12 <ismith@galois.com>
2025-12-19 15:38:57 -0500

    dwarf parsing error messages

---
+++ grease/src/Grease/Macaw/Dwarf/Diagnostic.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE LambdaCase #-}
+
+module Grease.Macaw.Dwarf.Diagnostic (Diagnostic (..), severity) where
+
+import Data.Macaw.Dwarf qualified as Dwarf
+import Grease.Diagnostic.Severity (Severity (..))
+import Prettyprinter qualified as PP
+
+data Diagnostic where
+  FailedToParse :: Dwarf.Subprogram -> String -> Diagnostic
+  deriving (Show)
+
+instance PP.Pretty Diagnostic where
+  pretty = \case
+    FailedToParse subprog msg -> PP.hcat [PP.pretty "Failed to parse dwarf shape in :", PP.viaShow subprog, PP.pretty " with message: ", PP.pretty msg]
+
+severity :: Diagnostic -> Severity
+severity = \case
+  FailedToParse _ _ -> Warn
