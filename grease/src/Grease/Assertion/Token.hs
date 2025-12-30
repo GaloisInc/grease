8fd50ca83575f217186787655f9f2d1b1a686f20
2over12 <ismith@galois.com>
2025-12-30 13:33:50 -0500

    start lexer

---
+++ grease/src/Grease/Assertion/Token.hs
@@ -0,0 +1,26 @@
+module Grease.Assertion.Token (Token (..)) where
+
+import Data.Word (Word64)
+
+data Token
+  = LParen
+  | RParen
+  | TokenBool Bool
+  | SepConj
+  | Comma
+  | TypeOf
+  | Zext
+  | Trunc
+  | Lt
+  | Lte
+  | Slt
+  | Slte
+  | Eq
+  | Exclam
+  | Ite
+  | LabelVar String
+  | ExistentialVar String
+  | ProgramVar String
+  | NaturalNum Int
+  | Bv
+  | HexVal Word64
