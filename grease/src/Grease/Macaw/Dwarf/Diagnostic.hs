a6210f3f86a98acb01ab66ae06091748b080e569
2over12 <ismith@galois.com>
2026-01-05 11:36:17 -0500

    DWARF: Add parsing shapes from DWARF arrays (#488)
    Adds DWARF parsing for arrays via the count TAG. Also cleans up dwarf parsing errors to use a pretty printable type as well as an enum with fields for the final diagnostic type

--- grease/src/Grease/Macaw/Dwarf/Diagnostic.hs
+++ grease/src/Grease/Macaw/Dwarf/Diagnostic.hs
@@ -1,25 +1,53 @@
 {-# LANGUAGE LambdaCase #-}
 
-module Grease.Macaw.Dwarf.Diagnostic (Diagnostic (..), severity) where
+module Grease.Macaw.Dwarf.Diagnostic (Diagnostic (..), severity, DwarfShapeParsingError (..)) where
 
+import Data.Macaw.Dwarf qualified as DWARF
 import Data.Macaw.Dwarf qualified as Dwarf
+import Data.Macaw.Types (TypeRepr)
 import Grease.Diagnostic.Severity (Severity (..))
+import Grease.Macaw.RegName
 import Prettyprinter qualified as PP
 
 data Diagnostic where
-  FailedToParse :: Dwarf.Subprogram -> String -> Diagnostic
-  deriving (Show)
+  FailedToParse :: (PP.Pretty a, Show a) => Dwarf.Subprogram -> Dwarf.Variable -> a -> Diagnostic
+
+deriving instance Show Diagnostic
 
 instance PP.Pretty Diagnostic where
   pretty = \case
-    FailedToParse subprog msg ->
+    FailedToParse subprog var msg ->
       PP.vcat
         [ PP.pretty "Failed to parse DWARF shape in:"
         , PP.viaShow subprog
-        , PP.pretty "DWARF message: "
+        , PP.pretty "Stopped at var: "
+        , PP.viaShow var
+        , PP.pretty "DWARF error message: "
         , PP.pretty msg
         ]
 
 severity :: Diagnostic -> Severity
 severity = \case
-  FailedToParse _ _ -> Warn
+  FailedToParse _ _ _ -> Warn
+
+data DwarfShapeParsingError where
+  UnsupportedType :: DWARF.TypeApp -> DwarfShapeParsingError
+  TypeMistmatchForRegister :: RegName -> String -> TypeRepr actual -> DwarfShapeParsingError
+  UnexpectedDWARFForm :: String -> DwarfShapeParsingError
+
+deriving instance Show DwarfShapeParsingError
+
+instance PP.Pretty DwarfShapeParsingError where
+  pretty = \case
+    UnsupportedType ty ->
+      PP.sep [PP.pretty "Unsupported DWARF type: ", PP.viaShow ty]
+    TypeMistmatchForRegister rname expectedType actualType ->
+      PP.sep
+        [ PP.pretty "At register "
+        , PP.viaShow rname
+        , PP.pretty "DWARF expected a register type of: "
+        , PP.viaShow expectedType
+        , PP.pretty " Macaw register type is: "
+        , PP.viaShow actualType
+        ]
+    UnexpectedDWARFForm msg -> PP.sep [PP.pretty "Unexpected format of DWARF: ", PP.pretty msg]
