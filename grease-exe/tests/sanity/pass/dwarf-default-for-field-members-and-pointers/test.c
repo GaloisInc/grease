af32140d5111d79044cd2943836a8437e3599b54
2over12 <ismith@galois.com>
2026-01-08 09:43:28 -0500

    DWARF defaults test case (#505)
    Adds a test case for adding default padding for types that fail to parse in structures and pointers

---
+++ grease-exe/tests/sanity/pass/dwarf-default-for-field-members-and-pointers/test.c
@@ -0,0 +1,22 @@
+/* Copyright (c) Galois, Inc. 2025 */
+
+// Tests that we can still succeed without heuristics to load fields prior to a
+// failure and after a function pointer
+
+typedef union y {
+  long x;
+  char z;
+} y;
+
+struct bar {
+  int (*somefn)(int);
+  int x;
+  y *blah;
+};
+
+int test(int x, int *y, struct bar *z) { return x + *y + z->x; }
+
+// all: flags {"--symbol", "test", "--use-debug-info-types", "--no-heuristics"}
+// x64: go(prog)
+// x64: check("Failed to parse DWARF shape in")
+// x64: ok()
