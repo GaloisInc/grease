52ec88450704c07c01d6114b00f2c4becdebc419
Langston Barrett <langston.barrett@gmail.com>
2026-01-09 16:00:34 -0500

    Add `-Werror=missing-import-lists` to `grease-exe.cabal`

--- grease-exe/src/Grease/Main.hs
+++ grease-exe/src/Grease/Main.hs
@@ -6,6 +6,8 @@
 {-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+-- TODO(#162)
+{-# OPTIONS_GHC -Wno-missing-import-lists #-}
 
 -- |
 -- Copyright        : (c) Galois, Inc. 2024
