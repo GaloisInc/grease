a0cab850811b06b3ea93254918eaae8496eb0ae1
2over12 <ismith@galois.com>
2026-01-13 13:38:41 -0500

    keep precise shapes

--- grease/src/Grease/Macaw/Shapes.hs
+++ grease/src/Grease/Macaw/Shapes.hs
@@ -87,7 +87,7 @@ macawInitArgShapes la bak archCtx opts parsed elf memory argNames mbCfgAddr = do
     case shouldUseDwarf of
       GO.NoDebugInfoShapes -> pure initArgs0
       GO.ConservativeDebugInfoShapes -> getDwarfArgs (Dwarf.ShouldUseConservativeDebugShapes True)
-      GO.PreciseDebugInfoShapes -> getDwarfArgs (Dwarf.ShouldUseConservativeDebugShapes True)
+      GO.PreciseDebugInfoShapes -> getDwarfArgs (Dwarf.ShouldUseConservativeDebugShapes False)
   pure $ do
     -- Either
     initArgs1 <-
